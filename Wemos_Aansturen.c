51f365124589b4a05f1c429a7efcc76df24ee1b5
IceFox2024 <168815657+IceFox2024@users.noreply.github.com>
2025-03-13 17:55:15 +0100

    Create Wemos_Aansturen.c

---
+++ Wemos_Aansturen.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>  // Voor inet_pton()
+#include <netinet/in.h> // Voor adressen
+#include <sys/socket.h> // Voor sockets
+#include <unistd.h>     // Voor close()
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define PORT 8888
+#define TIMEOUT 3  // Timeout in seconden
+#define Devices 2
+
+// IP-adressen van de Wemos-apparaten
+const char *wemos_ips[] = {
+    "192.168.137.141",  // Wemos 1
+    "192.168.137.206"   // Wemos 2
+};
+int client_fd;  // File descriptor voor socket
+struct sockaddr_in serv_addr;
+char buffer[1024] = {0};
+char *hello = "Hello from client\n";
+char *sluiten = "End\n";
+char *Led = "Led\n";
+char *Knop = "Knop\n";
+int Waarde_Knop = 0;
+int Socket = 0;
+bool Versturen = false;
+
+int connect_to_wemos(const char *ip) {
+    int sock;
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Socket creation error\n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
+        printf("Ongeldig adres of niet ondersteund: %s\n", ip);
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("Verbinding mislukt met: %s\n", ip);
+        return -1;
+    }
+
+    printf("Verbonden met Wemos op %s\n", ip);
+    return sock;
+}
+
+int vraag_knop_status(int Socket) {
+    client_fd = connect_to_wemos(wemos_ips[Socket]);
+    send(client_fd, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+    read(client_fd, buffer, sizeof(buffer) - 1);
+    printf("%s\n", buffer);
+    memset(buffer, 0, sizeof(buffer));
+    sleep(1);
+    send(client_fd, Knop, strlen(Knop), 0);
+    read(client_fd, buffer, sizeof(buffer) - 1);
+    printf("%s\n", buffer);
+    memset(buffer, 0, sizeof(buffer));
+
+    read(client_fd, buffer, sizeof(buffer) - 1);
+    printf("%s\n", buffer);
+    if (strcmp(buffer, "TRUE") == 0) {
+        printf("Connectie naar andere\n");
+        Waarde_Knop = 1;
+    }
+    sleep(1);
+    memset(buffer, 0, sizeof(buffer));
+    while (strcmp(buffer, "Verbinding wordt afgesloten door server") != 0)
+    {
+        send(client_fd, sluiten, strlen(sluiten), 0);
+        read(client_fd, buffer, sizeof(buffer) - 1);
+        printf("End message sent\n");
+        sleep(2);
+    }
+}
+int main() {
+    while (1) {
+        for(Socket =0;Socket < Devices; Socket++ ){
+        printf("%d\n", Socket);
+        vraag_knop_status(Socket);
+        // zet_led_aan();
+        sleep(3);  // Wacht even voordat de volgende check start
+    }
+}
+    return 0;
+}
