d865cae2c0bdee8dd881a9cbfbf706b6d64acf6d
juulvk <23093293@student.hhs.nl>
2025-04-04 15:17:27 +0200

    extra button toegevoegd
    voor de poc nog een extra knop toegevoegd in de code, als hij ingedrukt is geeft hij status 3 aan de pi.

--- Slave stm32/mainslave/slaveknop.ino
+++ Slave stm32/mainslave/slaveknop.ino
@@ -3,25 +3,37 @@
 #include <stdlib.h>
 #include <Wire.h>
 
-#define BUTTON_PIN PA0 
+#define BUTTON_PIN PA0
+#define BUTTON_PIN2 PA4 
 
 void knopSetup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-
+  pinMode(BUTTON_PIN2, INPUT_PULLUP);
 }
 
 int leesstatus() {  // Lees de status van de knop (de pin is LOW als de knop is ingedrukt)
-    bool buttonState = digitalRead(BUTTON_PIN);
+    bool buttonState1 = digitalRead(BUTTON_PIN);
+    bool buttonState2 = digitalRead(BUTTON_PIN2);
 
-    if (buttonState == LOW) {
-    delay(300);  // Debouncing voor de knop
-    if (digitalRead(BUTTON_PIN) == LOW) {
-      Slave1_Knop_Waarde = 1;
-      Serial.println("Knop ingedrukt");
+    if (buttonState1 == LOW) {
+        delay(150);  // Debouncing voor knop 1
+        if (digitalRead(BUTTON_PIN) == LOW) {
+            Slave1_Knop_Waarde = 1;
+            Serial.println("Knop 1 ingedrukt");
+            return Slave1_Knop_Waarde;
+        }
     }
-  } else {
-    Slave1_Knop_Waarde = 0;
-  }
 
+    if (buttonState2 == LOW) {
+        delay(150);  // Debouncing voor knop 2
+        if (digitalRead(BUTTON_PIN2) == LOW) {
+            Slave1_Knop_Waarde = 3;
+            Serial.println("Knop 2 ingedrukt");
+            return Slave1_Knop_Waarde;
+        }
+    }
+
+    // Geen van beide knoppen ingedrukt
+    Slave1_Knop_Waarde = 0;
     return Slave1_Knop_Waarde;
 }
