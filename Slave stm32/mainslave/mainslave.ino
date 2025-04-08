143b8da7474e3bec73a1a550a9d3af10a589912e
juulvk <23093293@student.hhs.nl>
2025-04-08 11:59:19 +0200

    Wire.onRequest teruggezet na kleine research

--- Slave stm32/mainslave/mainslave.ino
+++ Slave stm32/mainslave/mainslave.ino
@@ -8,9 +8,9 @@ void setup() {
 
   I2Csetup(adresslave);     //I2c verbinding initialiseren met de SDA op BP7 en de SCL op PB6, met als adres 0x11
   knopSetup();              //de knop initialiseren
+  Wire.onRequest(requestData);  //als er data wordt gevraagd data geven, Wire regelt dat na deze regel de slave reageerd op een master zonder dit in de while te hoeven loopen.
 }
 
 void loop() {
   Slave1_Knop_Waarde = leesstatus();
-  Wire.onRequest(requestData);  //als er data wordt gevraagd data geven
 }
