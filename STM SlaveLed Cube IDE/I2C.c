0f5859fe8248c31598134721d4f933e9cb2ae9ef
juulvk <23093293@student.hhs.nl>
2025-04-24 16:39:31 +0200

    verbeterde I2C
    werkende I2C op de STM32 cube IDE

--- STM SlaveLed Cube IDE/I2C.c
+++ STM SlaveLed Cube IDE/I2C.c
@@ -1,19 +1,21 @@
 #include "I2c.h"
 
 void LedSlave() {
-	uint8_t data = 0;
 
-	while(1) {
-		HAL_I2C_Slave_Receive(&hi2c1, &data, 1, HAL_MAX_DELAY);
-		if (data == 1) {
-		HAL_GPIO_WritePin(LEDJE_GPIO_Port, LEDJE_Pin, 1);
-		HAL_Delay(500);
-		}
-		if (data == 0) {
-		HAL_GPIO_WritePin(LEDJE_GPIO_Port, LEDJE_Pin, 0);
-		HAL_Delay(500);
-		}
+    while (1) {
+    	uint8_t RX_Buffer = 0;
 
-	}
+    		  if (HAL_I2C_Slave_Receive(&hi2c1, &RX_Buffer, 1, 1000) == HAL_OK) {
+    		      char buffer[50];
+    		      sprintf(buffer, "Ontvangen data: %d\r\n", RX_Buffer);
 
+    		      if (RX_Buffer == 1) {
+    		          HAL_GPIO_WritePin(LEDJE_GPIO_Port, LEDJE_Pin, GPIO_PIN_SET);
+    		      } else if (RX_Buffer == 0) {
+    		          HAL_GPIO_WritePin(LEDJE_GPIO_Port, LEDJE_Pin, GPIO_PIN_RESET);
+    		      }
+    		  }
+
+    }
 }
+
--- STM SlaveLed Cube IDE/main.c
+++ STM SlaveLed Cube IDE/main.c
@@ -43,16 +43,18 @@
 
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
+DMA_HandleTypeDef hdma_i2c1_rx;
 
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-
+uint8_t RX_Buffer [1] ; // DATA to receive
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
@@ -93,6 +95,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USART2_UART_Init();
   MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
@@ -104,6 +107,7 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -187,8 +191,8 @@ static void MX_I2C1_Init(void)
 
   /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
-  hi2c1.Init.Timing = 0x00B07CB4;
-  hi2c1.Init.OwnAddress1 = 16;
+  hi2c1.Init.Timing = 0x0040040C;
+  hi2c1.Init.OwnAddress1 = 22;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
   hi2c1.Init.OwnAddress2 = 0;
@@ -213,6 +217,10 @@ static void MX_I2C1_Init(void)
   {
     Error_Handler();
   }
+
+  /** I2C Fast mode Plus enable
+  */
+  HAL_I2CEx_EnableFastModePlus(I2C_FASTMODEPLUS_I2C1);
   /* USER CODE BEGIN I2C1_Init 2 */
 
   /* USER CODE END I2C1_Init 2 */
@@ -251,6 +259,22 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel7_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel7_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel7_IRQn);
 
 }
 
