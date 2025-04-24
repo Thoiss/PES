740dfd57f7a95a6a5c9653a223c198ba428236bb
Thijs van der Zwan <115693279+Thoiss@users.noreply.github.com>
2025-04-24 18:02:08 +0200

    Update globals.ino
    wifi variabelen toegevoegd voor statisch ip

--- Wemos_Code_Pro/globals.ino
+++ Wemos_Code_Pro/globals.ino
@@ -9,3 +9,10 @@ int Print_Knop = 0;
 int PiPrint_Knop = 0;
 int RGB_Preset_Knop = 0;
 int RGBWaarde = 0;
+
+// WiFi
+const char* ssid = "pinetwerk";
+const char* password = "bok12345";
+IPAddress ip(192, 168, 137, 20);        // IP van deze Wemos
+IPAddress gateway(192, 168, 137, 1);    // IP van de Raspberry Pi (de gateway)
+IPAddress subnet(255, 255, 255, 0);   // Subnetmasker
