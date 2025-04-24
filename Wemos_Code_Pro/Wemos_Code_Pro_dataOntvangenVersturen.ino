58d7cdd36e07d3a1c1a7740104eb9768e8c2310b
IceFox2024 <23094214@student.hhs.nl>
2025-04-24 18:14:01 +0200

    Update Wemos_Code_Pro_dataOntvangenVersturen.ino

--- Wemos_Code_Pro/Wemos_Code_Pro_dataOntvangenVersturen.ino
+++ Wemos_Code_Pro/Wemos_Code_Pro_dataOntvangenVersturen.ino
@@ -19,14 +19,12 @@ void dataOntvangenVersturen() {
             if (Print_Knop == 1) {
               Serial.print("Op data is knop uitgelezen");
               strcat(Status, " TRUE");
-              Print_Knop = 0;
             } else {
               strcat(Status, " FALSE");
             }
             if (PiPrint_Knop == 1) {
               Serial.print("Op data is knop uitgelezen");
               strcat(Status, " PiTRUE");
-              PiPrint_Knop = 0;
             } else {
               strcat(Status, " PiFALSE");
             }
@@ -38,7 +36,6 @@ void dataOntvangenVersturen() {
               Serial.printf("RGB Waarde string: %s\n", RGBWaardeStr);
               strcat(Status, RGBWaardeStr);
               Serial.printf("Status: %s\n", Status);
-              RGB_Preset_Knop = 0;
             } else {
               strcat(Status, " RGBFALSE");
             }
@@ -47,6 +44,17 @@ void dataOntvangenVersturen() {
           if (data == "Hello from client") {
             clients[i].print(deviceNaam);
           }
+          if (data == "LED_ACK") {
+            Print_Knop = 0;
+          }
+          if (data == "PILED_ACK") {
+            PiPrint_Knop = 0;
+          }
+          if (data == "RGB_ACK") {
+            RGB_Preset_Knop = 0;
+            ;
+          }
+
           if (data == "End") {
             clients[i].print("Verbinding wordt afgesloten door server");
             Serial.print("Client ");
