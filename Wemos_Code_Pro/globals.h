e707fbeb9632ddaa630cd6dceb0dc44c4070f9f4
IceFox2024 <168815657+IceFox2024@users.noreply.github.com>
2025-04-24 16:45:35 +0200

    Add files via upload

---
+++ Wemos_Code_Pro/globals.h
@@ -0,0 +1,22 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <ESP8266WiFi.h>
+
+#define MAX_CLIENTS 5                               // Aantal toegestane clients
+#define Time_Delay 10000                            // 10 seconden timeout
+#define BUTTON_PIN D1                               // Knop op D1 (GPIO5)
+#define PI_PIN D2                                   // Knop op D2 (GPIO4)
+#define RGB_Button D5                               // RGB_Button op D5 (GPIO14)
+
+extern WiFiServer server;
+extern char Status[200];
+extern WiFiClient clients[MAX_CLIENTS];
+extern unsigned long lastActiveTime[MAX_CLIENTS];
+
+extern int Print_Knop;
+extern int PiPrint_Knop;
+extern int RGB_Preset_Knop;
+extern int RGBWaarde;
+
+#endif  // GLOBALS_H
