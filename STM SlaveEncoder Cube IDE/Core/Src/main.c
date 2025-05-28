e962d9d186b76f5dc9c215143788fb094e9b53bc
IceFox2024 <23094214@student.hhs.nl>
2025-05-28 16:43:18 +0200

    Timer 6 toegevoegd voor debouncing

--- STM SlaveEncoder Cube IDE/Core/Src/main.c
+++ STM SlaveEncoder Cube IDE/Core/Src/main.c
@@ -43,6 +43,7 @@
 I2C_HandleTypeDef hi2c1;
 
 TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim6;
 
 UART_HandleTypeDef huart2;
 
@@ -51,11 +52,12 @@ uint8_t RX_Buffer [1] ;
 char buffer[20];
 char tekstbuffer[50];
 char Test[] = "test\n";
+char Verstuurd[] = "Verstuurd\n";
 uint32_t rawCounter = 0;
 uint32_t delay = 200;
 uint8_t knopStatus = 0; // Variabele om de knopstatus op te slaan
 uint8_t servoKnop = 0;
-
+uint8_t knopvergrendel =0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -64,6 +66,7 @@ static void MX_GPIO_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_I2C1_Init(void);
+static void MX_TIM6_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -104,18 +107,22 @@ int main(void)
   MX_TIM2_Init();
   MX_USART2_UART_Init();
   MX_I2C1_Init();
+  MX_TIM6_Init();
   /* USER CODE BEGIN 2 */
   	HAL_I2C_Slave_Receive_IT(&hi2c1, RX_Buffer, 1);
 	HAL_TIM_Encoder_Start(&htim2, TIM_CHANNEL_ALL);
-
+	HAL_TIM_Base_Start_IT(&htim6);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
 	while (1) {
 		rawCounter = counterLezen();
-		if(knopStatus == 0){
+		if(knopStatus == 0 && knopvergrendel == 0){
 		knopStatus = encoderKnopLezen();
+		}
+		if(knopStatus == 1){
+			knopvergrendel = 1;
 		}
 
 		if(servoKnop == 0){
@@ -289,6 +296,44 @@ static void MX_TIM2_Init(void)
 }
 
 /**
+  * @brief TIM6 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM6_Init(void)
+{
+
+  /* USER CODE BEGIN TIM6_Init 0 */
+
+  /* USER CODE END TIM6_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM6_Init 1 */
+
+  /* USER CODE END TIM6_Init 1 */
+  htim6.Instance = TIM6;
+  htim6.Init.Prescaler = 79;
+  htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim6.Init.Period = 999;
+  htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim6, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM6_Init 2 */
+
+  /* USER CODE END TIM6_Init 2 */
+
+}
+
+/**
   * @brief USART2 Initialization Function
   * @param None
   * @retval None
@@ -381,6 +426,7 @@ if(RX_Buffer [0]== 1){
   	        }
 
 }
+
 else if(RX_Buffer [0]== 2){
 
   	        uint8_t commando = RX_Buffer[0];
@@ -390,9 +436,10 @@ else if(RX_Buffer [0]== 2){
   	        {
   	            // stuur de gevraagde byte terug
   	          HAL_I2C_Slave_Transmit_IT(&hi2c1, &knopStatus, 1);
-//  	          HAL_UART_Transmit(&huart2, (uint8_t*) Test, strlen(Test), 1000);
+  	        HAL_UART_Transmit(&huart2, (uint8_t*) Verstuurd, strlen(Verstuurd), 1000);
 
   	        }
+  	      knopStatus =0;
 
 }
 else if(RX_Buffer [0]== 3){
@@ -410,7 +457,7 @@ else if(RX_Buffer [0]== 3){
 }
 else if(RX_Buffer [0]== 4){
 	servoKnop = 0;
-	knopStatus =0;
+	//knopStatus =0;
 	HAL_UART_Transmit(&huart2, (uint8_t*) Test, strlen(Test), 1000);
 }
     HAL_I2C_Slave_Receive_IT(&hi2c1, RX_Buffer, 1);
@@ -421,6 +468,11 @@ void HAL_I2C_SlaveTxCpltCallback(I2C_HandleTypeDef *hi2c)
 {
     if (hi2c->Instance == I2C1) {
         HAL_I2C_Slave_Receive_IT(&hi2c1, RX_Buffer, 1);
+    }
+}
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+    if (htim->Instance == TIM6) {
+    	knopvergrendel = 0;
     }
 }
 /* USER CODE END 4 */
