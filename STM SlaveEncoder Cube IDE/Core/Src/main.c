601a4cb95885ca5a93e6667d4a40793f02dfb1a8
IceFox2024 <23094214@student.hhs.nl>
2025-06-03 17:26:15 +0200

    Revert "Variabele bufferknop toegevoegd"
    This reverts commit 9290522c8231be6ffc1bbd7fe95a65b5d50e8a21.

--- STM SlaveEncoder Cube IDE/Core/Src/main.c
+++ STM SlaveEncoder Cube IDE/Core/Src/main.c
@@ -55,7 +55,6 @@ char Verstuurd[] = "Verstuurd\n";
 uint32_t rawCounter = 0;
 uint32_t delay = 200;
 uint8_t knopStatus = 0; // Variabele om de knopstatus op te slaan
-int bufferknop =0;
 uint8_t servoKnop = 0;
 /* USER CODE END PV */
 
@@ -115,9 +114,6 @@ int main(void)
 	while (1) {
 		rawCounter = counterLezen();
 		knopStatus = encoderKnopLezen();
-		if (knopStatus ==1){
-			bufferknop =1;
-		}
 		if(servoKnop == 0){
 		servoKnop = servoKnopLezen();
 		}
@@ -390,11 +386,11 @@ else if(RX_Buffer [0]== 2){
   	        if (commando >= 1 && commando <= 4)
   	        {
   	            // stuur de gevraagde byte terug
-  	          HAL_I2C_Slave_Transmit_IT(&hi2c1, &bufferknop, 1);
+  	          HAL_I2C_Slave_Transmit_IT(&hi2c1, &knopStatus, 1);
   	        HAL_UART_Transmit(&huart2, (uint8_t*) Verstuurd, strlen(Verstuurd), 1000);
 
   	        }
-  	      bufferknop =0;
+  	      knopStatus =0;
 
 }
 else if(RX_Buffer [0]== 3){
